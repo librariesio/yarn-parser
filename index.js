ec09b2e65fd374fb05c18ebf2052a9095ad61c4b
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-20 13:11:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var yarn = require('yarn/lib/lockfile/wrapper.js')
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.text());
+app.disable('x-powered-by');
+
+app.use(function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.post("/parse/", bodyParser.text({type: '*/*'}), function(req,res){
+  var dependencies = yarn.parse(str)
+  var deps = []
+  Object.keys(dependencies).forEach((dep) => {
+    deps.push({
+      name: dep.split('@')[0],
+      version: dependencies[dep].version,
+      type: 'runtime'
+    })
+  })
+
+  res.json(deps)
+});
+
+app.use(function(err, req, res, next) {
+  console.error('ERR:', err);
+  console.error('STACK:', err.stack);
+  res.status(500).send({error: 'Something went wrong.'});
+});
+
+app.listen(port, function() {
+  console.log('Listening on', port);
+});
