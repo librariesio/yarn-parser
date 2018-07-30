484d9b262bb7b25fd44f81ec3c918881e7e19020
Matt Ellis <mellis@tidelift.com>
2018-07-30 15:49:25 -0400

    Update test method

--- test/deps-test.js
+++ test/deps-test.js
@@ -12,7 +12,9 @@ it('Parses body', function(done) {
 it('Correctly parses @ symbol as first character', function(done) {
   var tmp = fs.readFileSync('./test/fixtures/yarn.lock', 'utf-8');
   var deps = manifestParser.parseDependencies(tmp);
-  expect(deps[0].name).to.equal("@yarnpkg/lockfile");
-  expect(deps[0].version).to.equal("1.0.0");
+  expect(deps[0].name).to.equal('@yarnpkg/lockfile');
+  expect(deps[0].version).to.equal('1.0.0');
+  expect(deps[1].name).to.equal('accepts');
+  expect(deps[2].name).to.equal('ajv');
   done();
 });
