925c662c764f6a135aa3ad59a059c90be75095e4
John Bintz <johnbintz@tidelift.com>
2020-09-04 11:09:16 -0400

    Better @ handling in requirements

--- test/deps-test.js
+++ test/deps-test.js
@@ -1,40 +1,58 @@
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
 var request = require('request');
-var manifestParser = require('../lib/manifest-parser');
 var fs = require('fs')
 
-it('Parses body', function(done) {
-  var tmp = fs.readFileSync('./test/fixtures/yarn.lock', 'utf-8');
-  var deps = manifestParser.parseDependencies(tmp);
-  expect(deps.length).to.equal(140);
-  done();
+var manifestParser = require('../lib/manifest-parser');
+
+describe('splitNameAndVersionString', function() {
+  it('Correctly parses packages with @ symbol in name', function(done) {
+    expect(manifestParser.getNameFromVersionString('@yarnpkg/lockfile@^1.0.0')).to.equal('@yarnpkg/lockfile');
+    expect(manifestParser.getNameFromVersionString('@babel/code-frame@7.0.0-beta.44')).to.equal('@babel/code-frame');
+    done();
+  });
+  it('Correctly parses packages with plain old names', function(done) {
+    expect(manifestParser.getNameFromVersionString('uuid@~1.1.2')).to.equal('uuid');
+    done();
+  });
+  it('Correctly parses version requirments for git repos', function(done) {
+    expect(
+      manifestParser.getVersionFromVersionString(
+        'private-dep@https://github.com/myorg/private-dep.git#v1.2.3'
+      )
+    ).to.equal('https://github.com/myorg/private-dep.git#v1.2.3');
+    expect(
+      manifestParser.getVersionFromVersionString(
+        'private-dep@git+ssh://git@github.com/myorg/private-dep.git#v1.2.3'
+      )
+    ).to.equal('git+ssh://git@github.com/myorg/private-dep.git#v1.2.3');
+    done();
+  });
 });
 
-it('Correctly parses yarn.lock', function(done) {
-  var tmp = fs.readFileSync('./test/fixtures/yarn.lock', 'utf-8');
-  var deps = manifestParser.parseDependencies(tmp);
-  expect(deps[0].name).to.equal('@yarnpkg/lockfile');
-  expect(deps[0].version).to.equal('1.0.0');
-  expect(deps[1].name).to.equal('accepts');
-  expect(deps[2].name).to.equal('ajv');
-  done();
-});
-it('Correctly parses packages with @ symbol in name', function(done) {
-  expect(manifestParser.getNameFromVersionString('@yarnpkg/lockfile@^1.0.0')).to.equal('@yarnpkg/lockfile');
-  expect(manifestParser.getNameFromVersionString('@babel/code-frame@7.0.0-beta.44')).to.equal('@babel/code-frame');
-  done();
+describe('parsing', function() {
+  it('Parses body', function(done) {
+    var tmp = fs.readFileSync('./test/fixtures/yarn.lock', 'utf-8');
+    var deps = manifestParser.parseDependencies(tmp);
+    expect(deps.length).to.equal(140);
+    done();
+  });
+
+  it('Correctly parses yarn.lock', function(done) {
+    var tmp = fs.readFileSync('./test/fixtures/yarn.lock', 'utf-8');
+    var deps = manifestParser.parseDependencies(tmp);
+    expect(deps[0].name).to.equal('@yarnpkg/lockfile');
+    expect(deps[0].version).to.equal('1.0.0');
+    expect(deps[1].name).to.equal('accepts');
+    expect(deps[2].name).to.equal('ajv');
+    done();
+  });
+  it('parses a yarn.lock with a git reference', function(done) {
+    var tmp = fs.readFileSync('./test/fixtures/yarn-with-git-repo.lock', 'utf-8');
+    var deps = manifestParser.parseDependencies(tmp);
+    expect(deps[0].name).to.equal('vue');
+    expect(deps[0].version).to.equal('2.6.12');
+    expect(deps[0].requirement).to.equal('https://github.com/vuejs/vue.git#v2.6.12');
+    done();
+  });
 });
 
-it('Correctly parses packages with plain old names', function(done) {
-  expect(manifestParser.getNameFromVersionString('uuid@~1.1.2')).to.equal('uuid');
-  done();
-});
-
-it('parses a yarn.lock with a git reference', function(done) {
-  var tmp = fs.readFileSync('./test/fixtures/yarn-with-git-repo.lock', 'utf-8');
-  var deps = manifestParser.parseDependencies(tmp);
-  expect(deps[0].name).to.equal('vue');
-  expect(deps[0].version).to.equal('2.6.12');
-  expect(deps[0].requirement).to.equal('https://github.com/vuejs/vue.git#v2.6.12');
-  done();
-})
